a3d52fd47836772364b4e453bcd0fa14b7b65034
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2023-07-03 11:27:24 -0300

    rename fields

--- db/CreateDatabsase.sql
+++ db/CreateDatabsase.sql
@@ -30,7 +30,7 @@ CREATE TABLE itens (
   item_name VARCHAR(100) NOT NULL,
   item_price FLOAT NOT NULL,
   item_description VARCHAR(256),
-  item_image VARCHAR(256),
+  item_img_url VARCHAR(256),
   item_type_id INT NOT NULL,
   FOREIGN KEY (item_type_id) REFERENCES item_type_enum(id)
 );
@@ -84,10 +84,10 @@ INSERT INTO item_type_enum (type_name) VALUES ('Acompanhamento');
 INSERT INTO item_type_enum (type_name) VALUES ('Sobremesa');
 
 -- CADASTRANDO UM ITEM DE CADA CATEGORIA
-INSERT INTO itens (item_type_id, item_name, item_description, item_image, item_price) VALUES (1, 'Coca-cola', 'Refrigerante sabor cola', 'https://www.shutterstock.com/pt/image-vector/novi-sad-serbia-january-21-2018-1008479416', 5.90);
-INSERT INTO itens (item_type_id, item_name, item_description, item_image, item_price) VALUES (2, 'X-Tudo', 'Pão, Hamburguer e salada', 'https://www.shutterstock.com/pt/image-vector/3d-rendering-delicious-cheese-burger-2168020455', 16.90);
-INSERT INTO itens (item_type_id, item_name, item_description, item_image, item_price) VALUES (3, 'Batata-Frita', 'Batata-frita 200gms', 'https://www.shutterstock.com/pt/image-photo/french-fries-510881971', 9.90);
-INSERT INTO itens (item_type_id, item_name, item_description, item_image, item_price) VALUES (4, 'Sorvete de Morango', 'Delicioso sorvete de morango', 'https://www.shutterstock.com/pt/image-photo/ice-cream-cone-vanilla-strawberry-flavors-645817927', 2.90);
+INSERT INTO itens (item_type_id, item_name, item_description, item_img_url, item_price) VALUES (1, 'Coca-cola', 'Refrigerante sabor cola', 'https://www.shutterstock.com/pt/image-vector/novi-sad-serbia-january-21-2018-1008479416', 5.90);
+INSERT INTO itens (item_type_id, item_name, item_description, item_img_url, item_price) VALUES (2, 'X-Tudo', 'Pão, Hamburguer e salada', 'https://www.shutterstock.com/pt/image-vector/3d-rendering-delicious-cheese-burger-2168020455', 16.90);
+INSERT INTO itens (item_type_id, item_name, item_description, item_img_url, item_price) VALUES (3, 'Batata-Frita', 'Batata-frita 200gms', 'https://www.shutterstock.com/pt/image-photo/french-fries-510881971', 9.90);
+INSERT INTO itens (item_type_id, item_name, item_description, item_img_url, item_price) VALUES (4, 'Sorvete de Morango', 'Delicioso sorvete de morango', 'https://www.shutterstock.com/pt/image-photo/ice-cream-cone-vanilla-strawberry-flavors-645817927', 2.90);
 
 -- CADASTRANDO UM PEDIDO COM A DATA ATUAL PARA O PRIMEIRO CLIENTE
 INSERT INTO orders (order_date, order_total, customer_id) VALUES (current_date(), 35.60, 1);
