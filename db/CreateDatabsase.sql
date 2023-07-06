8ab0303a97336f52412f74ee3a1378ce7baf737b
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2023-07-06 20:22:05 -0300

    fields of customer is nullable and create default customer

--- db/CreateDatabsase.sql
+++ db/CreateDatabsase.sql
@@ -4,8 +4,8 @@ USE DeliverySystem;
 CREATE TABLE customers (
     id INT auto_increment PRIMARY KEY,
     customer_cpf VARCHAR(11) UNIQUE,
-    customer_name VARCHAR(100) NOT NULL,
-    customer_email VARCHAR(100) NOT NULL,
+    customer_name VARCHAR(100),
+    customer_email VARCHAR(100),
     is_active BOOLEAN NOT NULL
 );
 
@@ -66,6 +66,9 @@ SET character_set_client = utf8;
 SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
+
+-- CADASTRANDO CLIENTE DEFAULT
+INSERT INTO customers (id, customer_name, is_active) VALUES (1, 'unamed_customer', 1);
 
 -- CADASTRANDO 1 CLIENTE
 INSERT INTO customers (customer_name, customer_email, customer_cpf, is_active) VALUES ('Gabriel de Barros Pontes', 'gabrielpontes98@gmail.com', '46272039859', 1);
