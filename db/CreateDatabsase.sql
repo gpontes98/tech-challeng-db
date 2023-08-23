88fb3622e1eee460a72e7a9be8467ab68d7702ac
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2023-08-23 17:17:02 -0300

    feat: add payment table

--- db/CreateDatabsase.sql
+++ db/CreateDatabsase.sql
@@ -61,6 +61,30 @@ CREATE TABLE order_queue (
   FOREIGN KEY (status_queue_enum_id) REFERENCES status_queue_enum(id)
 );
 
+-- Esta tabela armazena os diferentes status possíveis para o pagamento.
+CREATE TABLE status_payment_enum (
+  id INT auto_increment PRIMARY KEY,
+  status_payment VARCHAR(100) NOT NULL
+);
+
+-- Esta tabela armazena os diferentes métodos possíveis para o pagamento.
+CREATE TABLE payment_method_enum (
+  id INT auto_increment PRIMARY KEY,
+  payment_method VARCHAR(100) NOT NULL
+);
+
+-- Esta tabela armazena informações do pagamento do pedido
+CREATE TABLE order_payment (
+  id INT auto_increment PRIMARY KEY,
+  last_update DATETIME NOT NULL,
+  order_id INT NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(id),
+  status_payment_enum_id INT NOT NULL,
+  FOREIGN KEY (status_payment_enum_id) REFERENCES status_payment_enum(id),
+  payment_method_enum_id INT NOT NULL,
+  FOREIGN KEY (payment_method_enum_id) REFERENCES payment_method_enum(id)
+);
+
 -- Definindo a codificação de caracteres para UTF-8
 SET character_set_client = utf8;
 SET character_set_connection = utf8;
@@ -102,3 +126,16 @@ INSERT INTO order_item (order_id, item_id, order_item_qtd) VALUES (1, 4, 1);
 
 -- CADASTRANDO O PEDIDO NA TABELA DE FILA
 INSERT INTO order_queue (order_id, waiting_time, status_queue_enum_id) VALUES (1, '00:05:00', 1);
+
+-- CADASTRANDO OS STATUS DE PAGAMENTO
+INSERT INTO status_payment_enum (status_payment) VALUES ('Aprovado');
+INSERT INTO status_payment_enum (status_payment) VALUES ('Reprovado');
+
+-- CADASTRANDO AS FORMAS DE PAGAMENTO
+INSERT INTO payment_method_enum (payment_method) VALUES ('QrCode');
+INSERT INTO payment_method_enum (payment_method) VALUES ('Dinheiro');
+INSERT INTO payment_method_enum (payment_method) VALUES ('Crédito');
+INSERT INTO payment_method_enum (payment_method) VALUES ('Débito');
+
+-- CADASTRANDO UM PAGAMENTO DE PEDIDO
+INSERT INTO order_payment (last_update, order_id, status_payment_enum_id, payment_method_enum_id) VALUES (now(), 1, 1, 1)
